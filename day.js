e7fc3e45be40ff73251563e5c0e77d035b229706
juliahop <hop.julia@hotmail.com>
2018-10-26 14:08:23 +0200

    Add functions to day.js

---
+++ day.js
@@ -0,0 +1,10 @@
+// Return the day of the week for the given date.
+function dayName(date) {
+  const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  return daysOfTheWeek[date.getDay()];
+}
+
+// Return a greeting for the given date.
+function greeting(date) {
+  return `Hello, world! Happy ${dayName(now)}.`
+}
