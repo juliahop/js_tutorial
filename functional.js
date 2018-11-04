41d6194c9fb1b844025bafeebac15b7b83b8326e
juliahop <hop.julia@hotmail.com>
2018-11-04 22:39:03 +0100

    Add reduce function

--- functional.js
+++ functional.js
@@ -59,3 +59,49 @@ function twoWords(elements) {
   return elements.filter(element => element.split(/\s+/).length === 2);
 }
 console.log(twoWords(states));
+
+// Reduce
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+  let total = 0;
+  elements.forEach(function(n) {
+    total += n;
+  });
+  return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+  return elements.reduce((total, n) => {return total += n;});
+}
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+  let lengths = {};
+  elements.forEach(function(element) {
+    lengths[element] = element.length;
+  });
+  return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+  return elements.reduce((lengths, element) => {
+    lengths[element] = element.length;
+    return lengths;
+  }, {});
+}
+console.log(functionalLengths(states));
+
+// Exercise
+function multiplyAll(elements) {
+  return elements.reduce((total, n) => {
+    return total *= n;
+  }, 1);
+}
+console.log(multiplyAll(numbers));
