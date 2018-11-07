1b2edd8078484941784f6c3cc9b86cfc55b93375
juliahop <hop.julia@hotmail.com>
2018-11-07 22:22:36 +0100

    Add Prototypes inc. Exercise

--- palindrome.js
+++ palindrome.js
@@ -8,13 +8,33 @@ function Phrase(content) {
   this.content = content;
 
   // Makes the phrase LOUDER
-  this.louder = function() {
-    return this.content.toUpperCase()
+  // this.louder = function() {
+    // return this.content.toUpperCase()
+  // }
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
   }
 
   // Returns true for a palindrome, false otherwise
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === reverse(this.processedContent());
   }
 }
+
+// Defines a TranslatedPhrase object
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  }
+}
+
+TranslatedPhrase.prototype = new Phrase();
